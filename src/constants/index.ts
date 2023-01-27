20097d6f7758653eb6961272812931aac1ac112f
matheusgmc <matheus.geraldo1@outlook.com>
2023-01-27 13:56:41 -0400

    :recycle: all changed to enum

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,30 +1,35 @@
-export const MOB_CLASSES = ["mage", "warrior", "hunter"] as const;
+export enum MOB_CLASSES {
+  MAGE = "Mage",
+  WARRIOR = "Warrior",
+  HUNTER = "Hunter",
+}
 
-export const ITEM_SLOT_NAME = ["LEFT_HAND", "RIGHT_HAND", "HEAD"] as const;
+export enum ITEM_SLOT_NAME {
+  LEFT_HAND = "Left Hand",
+  RIGHT_HAND = "Right Hand",
+  HEAD = "Head",
+}
 
-export const ITEM_TYPE = ["Weapon", "Armor", "Consumable"] as const;
+export enum ITEM_TYPE {
+  WEAPON = "Weapon",
+  ARMOR = "Armor",
+  CONSUMABLE = "Consumable",
+}
 
-export const ITEM_CATEGORY_WEAPON_SWORDS = [
-  "Short Sword",
-  "Long Sword",
-  "Great Sword",
-] as const;
+export enum ITEM_CATEGORY_WEAPON {
+  // SWORDS
+  SHORT_SWORD = "Short Sword",
+  LONG_SWORD = "Long Sword",
+  GREAT_SWORD = "Great Sword",
 
-export const ITEM_CATEGORY_WEAPON_STAFFS = [
-  "Fire Staff",
-  "Ice Staff",
-  "Lightning Staff",
-] as const;
+  // STAFFS
+  FIRE_STAFF = "Fire Staff",
+  ICE_STAFF = "Ice Staff",
+  LIGHTNING_STAFF = "Lightning Staff",
 
-export const ITEM_CATEGORY_WEAPON_MAGIC_SWORDS = [
-  "Fire Sword",
-  "Ice Sword",
-  "Lightning Sword",
-  "Poisonous Sword",
-] as const;
-
-export const ITEM_CATEGORY = [
-  ...ITEM_CATEGORY_WEAPON_SWORDS,
-  ...ITEM_CATEGORY_WEAPON_MAGIC_SWORDS,
-  ...ITEM_CATEGORY_WEAPON_STAFFS,
-] as const;
+  // MAGIC SWORDS
+  FIRE_SWORD = "Fire Sword",
+  ICE_SWORD = "Ice Sword",
+  LIGHTNING_SWORD = "Lightning Sword",
+  POISONOUS_SWORD = "Poisonous Sword",
+}
