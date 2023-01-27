de4b23dd61e018505c7d7954e0cf95838f4cd330
matheusgmc <matheus.geraldo1@outlook.com>
2023-01-27 13:59:02 -0400

    :label: types changed to use emuns

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,5 +1,16 @@
-import { ITEM_CATEGORY, ITEM_TYPE } from "@/constants";
+import {
+  ITEM_CATEGORY_WEAPON,
+  ITEM_TYPE,
+  MOB_CLASSES,
+  ITEM_SLOT_NAME,
+} from "@/constants";
 
-export type TypeItemCategory = (typeof ITEM_CATEGORY)[number];
+// ITEM
+export type TypeItemCategory = keyof typeof ITEM_CATEGORY_WEAPON;
+export type TypeItemType = keyof typeof ITEM_TYPE;
 
-export type TypeItemType = (typeof ITEM_TYPE)[number];
+// MOB
+export type TypeMobClasses = keyof typeof MOB_CLASSES;
+
+// EQUIPMENT
+export type TypeItemSlotName = keyof typeof ITEM_SLOT_NAME;
