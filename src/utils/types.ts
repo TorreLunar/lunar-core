29a8e00c7293f9d7a71c984a9100211feb24897f
matheusgmc <matheus.geraldo1@outlook.com>
2023-01-28 02:58:32 -0400

    :label: class changed to job

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,16 +1,20 @@
 import {
   ITEM_CATEGORY_WEAPON,
   ITEM_TYPE,
-  MOB_CLASSES,
+  MOB_JOB,
   ITEM_SLOT_NAME,
 } from "@/constants";
+import { BaseItemArmorStat } from "@/structure/stats/BaseItemArmorStat";
+import { BaseItemWeaponStat } from "@/structure/stats/BaseItemWeaponStat";
 
 // ITEM
 export type TypeItemCategory = keyof typeof ITEM_CATEGORY_WEAPON;
 export type TypeItemType = keyof typeof ITEM_TYPE;
 
 // MOB
-export type TypeMobClasses = keyof typeof MOB_CLASSES;
+export type TypeMobJob = keyof typeof MOB_JOB;
 
 // EQUIPMENT
 export type TypeItemSlotName = keyof typeof ITEM_SLOT_NAME;
+
+export type TypeItemStats = BaseItemWeaponStat | BaseItemArmorStat;
