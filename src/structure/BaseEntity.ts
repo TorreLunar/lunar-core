78c77b57b100086b07e2909552fa7c970f6b6ecd
matheusgmc <matheus.geraldo1@outlook.com>
2023-01-31 04:46:57 -0400

    :sparkles: added job

--- src/structure/BaseEntity.ts
+++ src/structure/BaseEntity.ts
@@ -22,6 +22,7 @@ export class BaseEntity<Equipment = any> implements BaseEntityProps<Equipment> {
   constructor(data: BaseEntityProps<Equipment>) {
     this.name = data.name;
     this.health_points = data.health_points;
+    this.job = data.job;
     this.level = data.level;
     this.attributes = data.attributes;
     this.equipment = data.equipment;
