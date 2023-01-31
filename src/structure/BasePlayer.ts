4ab52e87d2ec239fa5cf371f8d40900e59f1a103
matheusgmc <matheus.geraldo1@outlook.com>
2023-01-31 04:43:05 -0400

    :label: removed readonly

--- src/structure/BasePlayer.ts
+++ src/structure/BasePlayer.ts
@@ -6,20 +6,14 @@ export type BasePlayerProps = BaseEntityProps<BaseEquipmentPlayer> & {
   skill_points: number;
 };
 
-export class BasePlayer extends BaseEntity<BaseEquipmentPlayer> {
-  readonly nation: string;
-  readonly skill_points: number;
+export class BasePlayer
+  extends BaseEntity<BaseEquipmentPlayer>
+  implements BasePlayerProps
+{
+  nation: string;
+  skill_points: number;
   constructor(data: BasePlayerProps) {
     super(data);
-    this.nation = data.nation;
-    this.skill_points = data.skill_points;
-    this._patch();
-  }
-  private _patch() {
-    if (!this.nation) return console.error("nation is invalid");
-
-    if (typeof this.skill_points !== "number")
-      return console.log("skill_points is invalid");
   }
 }
 
